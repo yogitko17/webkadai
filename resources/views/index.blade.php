b9a80e9821ea4f8bf6db5d8427a3cbcf4328254c
yogitko17 <ykkkaj3172231106yhh_0307@yahoo.co.jp>
2021-09-05 14:53:57 +0900

    message

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,100 +7,12 @@
   <script src="https://unpkg.com/dayjs"></script>
   <script src="https://unpkg.com/dayjs@1.7.7/locale/ja.js"></script>
   <title>@yield('title')</title>
-  <link rel="stylesheet" href="../../css/reset.css">
-  <link rel="stylesheet" href="../../css/todo.css">
+  <link rel="stylesheet" href="{{ asset('css/todo.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+  
 
 <style>
-    .outer-frame {
-      background-color: rgb(73, 7, 226);
-      width: 100vw;
-      height: 100vh;
-      position: relative;
-    }
-    .inner-frame {
-      width: 55vw;
-      height:cover;
-      padding: 30px;
-      position:absolute;
-      top:50%;
-      left:50%;
-      background-color: white;
-      border-radius: 20px;
-      transform: translate(-50%, -50%);
-    }
-    .inner-ttl{
-      font-size: 50px;
-    }
-    .todo-list_content{
-      display:flex;
-      justify-content:space-between;
-    }
-    .input-main-txt{
-      width:80%;
-      padding: 10px;
-      border-radius:3px;
-      border:1px solid rgb(230, 226, 238);
-      outline:none;
-      font-size:20px;
-    }
-    .input-btn{
-      font-weight:bold;
-      padding: 12px 20px;
-      border-radius:3px;
-      border:1px solid rgb(219, 54, 197);
-      color:rgb(219, 54, 197);
-      outline:none;
-      cursor: pointer;
-      font-size:18px;
-      background-color:white;
-    }
-    table{
-      text-align:center;
-      width:100%;
-    }
-    tr{
-      height:100px;
-    }
-    th{
-      font-size: 22px;
-    }
-    td{
-      font-size: 20px;
-    }
-
-    .input-update-txt{
-      width:90%;
-      padding: 10px;
-      border-radius:3px;
-      border:1px solid rgb(230, 226, 238);
-      outline:none;
-      font-size:20px;
-    }
-    .update-btn{
-      font-weight:bold;
-      padding: 12px 20px;
-      border-radius:3px;
-      border:1px solid rgb(224, 98, 60);
-      color:rgb(224, 98, 60);
-      outline:none;
-      cursor: pointer;
-      font-size:18px;
-      background-color:white;
-    }
-    .delete-btn{
-      font-weight:bold;
-      padding: 12px 20px;
-      border-radius:3px;
-      border:1px solid rgb(83, 235, 209);
-      color:rgb(83, 235, 209);
-      outline:none;
-      cursor: pointer;
-      font-size:18px;
-      background-color:white;
-    }
-    .table-date{
-      width:30%;
-    }
+    
 </style>
 <body>
     <div class="outer-frame">
